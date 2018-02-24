7853a01f0ba23c9f127635aefded855b3778adb0
Chris <kobayashi@teamspooky.com>
2018-02-23 19:46:18 -0600

    wtf symlinks dont work with gsimplecal?!

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ ln -sv ~/.dotfiles/.conkyrc ~
 ln -sv ~/.dotfiles/.vimrc ~
 ln -sv ~/.dotfiles/.xbindkeysrc ~
 ln -sv ~/.dotfiles/.config/compton/* ~/.config/compton/
-ln -sv ~/.dotfiles/.config/gsimplecal/* ~/.config/gsimplecal/
 ln -sv ~/.dotfiles/.config/i3/* ~/.config/i3/
 ln -sv ~/.dotfiles/.config/mpd/* ~/.config/mpd/
 ln -sv ~/.dotfiles/.config/mpv/* ~/.config/mpv/
@@ -13,3 +12,4 @@ ln -sv ~/.dotfiles/.config/polybar/* ~/.config/polybar/
 ln -sv ~/.dotfiles/.config/termite/* ~/.config/termite/
 ln -sv ~/.dotfiles/.config/redshift.conf ~/.config/
 ln -sv ~/.dotfiles/.irssi/* ~/.irssi/
+cp ~/.dotfiles/.config/gsimplecal/* ~/.config/gsimplecal/
