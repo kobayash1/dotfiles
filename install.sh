e8aa87aea04fa12a2fdf38144bca66c9ce2fd82a
Chris <kobayashi@teamspooky.com>
2018-04-21 16:51:57 -0500

    cosmetic change to install.sh

--- install.sh
+++ install.sh
@@ -1,34 +1,34 @@
 #!/bin/sh
-
 mkdir ~/.colors
 mkdir ~/.config
-mkdir ~/.irssi
-mkdir ~/.mpdcron
 mkdir ~/.config/compton
+mkdir ~/.config/dunst
+mkdir ~/.config/gsimplecal
 mkdir ~/.config/i3
 mkdir ~/.config/mpd
 mkdir ~/.config/mpv
 mkdir ~/.config/polybar
 mkdir ~/.config/termite
-mkdir ~/.config/gsimplecal
-mkdir ~/.config/dunst
+mkdir ~/.irssi
+mkdir ~/.mpdcron
 
-ln -sv ~/.dotfiles/.bashrc ~
 ln -sv ~/.dotfiles/.bash_profile ~
-ln -sv ~/.dotfiles/.conkyrc ~
-ln -sv ~/.dotfiles/.vimrc ~
-ln -sv ~/.dotfiles/.xbindkeysrc ~
-ln -sv ~/.dotfiles/.Xresources ~
+ln -sv ~/.dotfiles/.bashrc ~
 ln -sv ~/.dotfiles/.colors/* ~/.colors/
 ln -sv ~/.dotfiles/.config/compton/* ~/.config/compton/
 ln -sv ~/.dotfiles/.config/i3/* ~/.config/i3/
 ln -sv ~/.dotfiles/.config/mpd/* ~/.config/mpd/
 ln -sv ~/.dotfiles/.config/mpv/* ~/.config/mpv/
 ln -sv ~/.dotfiles/.config/polybar/* ~/.config/polybar/
-ln -sv ~/.dotfiles/.config/termite/* ~/.config/termite/
 ln -sv ~/.dotfiles/.config/redshift.conf ~/.config/
+ln -sv ~/.dotfiles/.config/termite/* ~/.config/termite/
+ln -sv ~/.dotfiles/.conkyrc ~
 ln -sv ~/.dotfiles/.irssi/* ~/.irssi/
+ln -sv ~/.dotfiles/.vimrc ~
+ln -sv ~/.dotfiles/.xbindkeysrc ~
+ln -sv ~/.dotfiles/.Xresources ~
+
+cp ~/.dotfiles/.config/dunst/dunstrc ~/.config/dunst/
 cp ~/.dotfiles/.config/gsimplecal/* ~/.config/gsimplecal/
 cp ~/.dotfiles/.config/mpdnotify.conf ~/.config/
-cp ~/.dotfiles/.config/dunst/dunstrc ~/.config/dunst/
 cp -r ~/.dotfiles/.mpdcron ~/
